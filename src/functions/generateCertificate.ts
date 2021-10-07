b3b3bf56c3c862090ef8f35d03100ab60ebc08e1
Natanael Lima <taelima1997@gmail.com>
2021-10-06 23:20:13 -0300

    add initial function handle generate certificate

---
+++ src/functions/generateCertificate.ts
@@ -0,0 +1,73 @@
+import path from "path";
+import handlebars from "handlebars";
+import fs from "fs";
+import dayjs from "dayjs";
+
+import { document } from "../utils/dynamodbClient";
+
+interface ICreateCertificate {
+  id: string;
+  name: string;
+  grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+const compile = async function (data: ITemplate) {
+  const filePath = path.join(
+    process.cwd(),
+    "src",
+    "templates",
+    "certificate.hbs"
+  );
+
+  const html = fs.readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)(data);
+};
+
+export const handle = async (event) => {
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
+
+  await document
+    .put({
+      TableName: "users_certificates",
+      Item: {
+        id,
+        name,
+        grade,
+      },
+    })
+    .promise();
+
+    const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+    const medal = fs.readFileSync(medalPath, "base64");
+  
+    const data: ITemplate = {
+      date: dayjs().format("DD/MM/YYYY"),
+      grade,
+      name,
+      id,
+      medal,
+    };
+  
+    // Gera o certificado
+    // Compilar usando handlebars
+    const content = await compile(data);
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: "Certificate created",
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+};
